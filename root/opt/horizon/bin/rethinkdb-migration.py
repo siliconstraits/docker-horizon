7cf16697399827738c7e5a56488bee1839717687
Tu A. Nguyen <mr.anhtu9889@gmail.com>
2017-01-17 16:53:50 +0700

    Update primary_key

--- root/opt/horizon/bin/rethinkdb-migration.py
+++ root/opt/horizon/bin/rethinkdb-migration.py
@@ -6,7 +6,7 @@ PROJECT_NAME=os.environ['HORIZON_PROJECT_NAME']
 
 r.connect("rethinkdb", 28015).repl()
 r.db_create(PROJECT_NAME).run()
-r.db(PROJECT_NAME).table_create("cars").run()
+r.db(PROJECT_NAME).table_create("cars", primary_key="car_no").run()
 r.db(PROJECT_NAME).table_create("orders").run()
 r.db(PROJECT_NAME).table_create("users").run()
 r.db(PROJECT_NAME).table_create("hz_collections").run()
